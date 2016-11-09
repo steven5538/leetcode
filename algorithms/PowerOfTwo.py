a64d2a439e43b090af198617c7d09ad1e2302eae
steven5538 <steven5538@gmail.com>
2016-11-09 23:19:16 +0800

    231. Power of Two

---
+++ algorithms/PowerOfTwo.py
@@ -0,0 +1,9 @@
+import math
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n <= 0:
+            return False
+
+        return math.log10(n) / math.log10(2) % 1 == 0
+        
